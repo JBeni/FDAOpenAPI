6bf0042c84a8278f9e62db668aed3d2f127e7fce
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-02-23 12:08:27 +0200

    global usings imports

---
+++ FDAOpenAPI.WebUI/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using FDAOpenAPI.WebUI.Responses;
+global using FDAOpenAPI.WebUI.Models.Common;
+global using FDAOpenAPI.WebUI.Models.Drug;
+global using FDAOpenAPI.WebUI.Models.Food;
+global using FDAOpenAPI.WebUI.Models.Tobacco;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Text.Json;
