8308acd80e3f65503bb8b2615c4b63c4e3bf69f7
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-02-23 12:09:01 +0200

    create tobacco models

---
+++ FDAOpenAPI.WebUI/Models/Tobacco/TobaccoResult.cs
@@ -0,0 +1,15 @@
+﻿namespace FDAOpenAPI.WebUI.Models.Tobacco
+{
+    public class TobaccoResult
+    {
+        public string Date_Submitted { get; set; }
+        public string NonUser_Affected { get; set; }
+        public List<string> Reported_Health_Problems { get; set; }
+        public long Number_Tobacco_Products { get; set; }
+        public long Report_Id { get; set; }
+        public long Number_Health_Problems { get; set; }
+        public List<string> Reported_Product_Problems { get; set; }
+        public List<string> Tobacco_Products { get; set; }
+        public long Number_Product_Problems { get; set; }
+    }
+}
