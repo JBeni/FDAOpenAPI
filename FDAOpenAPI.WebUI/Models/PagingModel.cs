caf3638ee527fe997b1a9f0219a30dbedce1c531
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-02-23 12:09:16 +0200

    remove files

--- FDAOpenAPI.WebUI/Models/PagingModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FDAOpenAPI.WebUI.Models
-{
-    public class PagingModel
-    {
-        public int Skip { get; set; }
-        public int Limit { get; set; }
-        public int Total { get; set; }
-    }
-}
