dce115c67fbc1b715dd97eb696a7418e327643ea
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-02-24 11:52:43 +0200

    update controllers

---
+++ FDAOpenAPI.WebUI/Controllers/FoodAdverseEventsController.cs
@@ -0,0 +1,93 @@
+﻿namespace FDAOpenAPI.WebUI.Controllers
+{
+    public class FoodAdverseEventsController : ApiControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        //https://api.fda.gov/food/enforcement.json?search=distribution_pattern:"nationwide"&limit=5
+
+        public FoodAdverseEventsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(_configuration["OpenFdaFoodEndpoint"])
+            };
+        }
+
+        [HttpGet("adverse-reactions")]
+        public async Task<IActionResult> GetData([FromQuery] int limitNumber)
+        {
+            var response = await _httpClient.GetAsync($"event.json?limit={limitNumber}");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("adverse-reactions-count")]
+        public async Task<IActionResult> GetCount([FromQuery] string search_field, string value)
+        {
+            _ = long.TryParse(value, out long parseResult);
+            string? query;
+            if (parseResult == 0)
+            {
+                query = $"{search_field.ToLower()}:{value}";
+            }
+            else
+            {
+                query = $"{search_field.ToLower()}:{parseResult}";
+            }
+
+            var response = await _httpClient.GetAsync($"event.json?search={query}&count=reactions.exact");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementCountResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("adverse-reactions-search-by-field")]
+        public async Task<IActionResult> GetSearchField([FromQuery] string search_field, string value, int limitNumber)
+        {
+            _ = long.TryParse(value, out long parseResult);
+            string? query;
+            if (parseResult == 0)
+            {
+                query = $"{search_field.ToLower()}:{value}";
+            }
+            else
+            {
+                query = $"{search_field.ToLower()}:{parseResult}";
+            }
+
+            var response = await _httpClient.GetAsync($"event.json?search={query}&limit={limitNumber}");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("adverse-reactions-search-by-date-range")]
+        public async Task<IActionResult> GetSearchByDateRange([FromQuery] string startDate, string endDate)
+        {
+            var response = await _httpClient.GetAsync($"?search=date_started:[{startDate.Replace("/", "")}+TO+{endDate.Replace("/", "")}]");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+    }
+}
---
+++ FDAOpenAPI.WebUI/Controllers/FoodEnforcementsController.cs
@@ -0,0 +1,143 @@
+﻿namespace FDAOpenAPI.WebUI.Controllers
+{
+    public class FoodEnforcementsController : ApiControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        //https://api.fda.gov/food/event.json?search=industry_name:"Snack Food Item"&limit=5
+
+        public FoodEnforcementsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(_configuration["OpenFdaFoodEndpoint"])
+            };
+        }
+
+        [HttpGet("foods")]
+        public async Task<IActionResult> GetData([FromQuery] int limitNumber)
+        {
+            var response = await _httpClient.GetAsync($"?limit={limitNumber}");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("foods-count")]
+        public async Task<IActionResult> GetCount([FromQuery] string search_field)
+        {
+            var response = await _httpClient.GetAsync($"?count={search_field}.exact");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementCountResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchData()
+        {
+            var response = await _httpClient.GetAsync($"?search=distribution_pattern:'nationwide'&limit=10");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<FoodRecallEnforcementResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("DownloadData")]
+        public async Task<IActionResult> DownloadFoodData()
+        {
+            HttpClient httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://download.open.fda.gov/food/enforcement/food-enforcement-0001-of-0001.json.zip")
+            };
+
+            var response = await httpClient.GetAsync("");
+            var responseResult = await response.Content.ReadAsStringAsync();
+
+            return Ok();
+        }
+
+
+
+
+
+
+/*
+        [HttpGet("tobaccos")]
+        public async Task<IActionResult> GetData([FromQuery] int limitNumber)
+        {
+            var response = await _httpClient.GetAsync($"?limit={limitNumber}");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<TobaccoResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("tobaccos-search-by-field")]
+        public async Task<IActionResult> SearchProblemByDateRange([FromQuery] string search_field, string value)
+        {
+            _ = long.TryParse(value, out long parseResult);
+            string? query;
+            if (parseResult == 0)
+            {
+                query = $"{search_field.ToLower()}:{value}";
+            }
+            else
+            {
+                query = $"{search_field.ToLower()}:{parseResult}";
+            }
+
+            var response = await _httpClient.GetAsync($"?search={query}");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<TobaccoResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("tobaccos-by-date-range")]
+        public async Task<IActionResult> SearchProblemByDateRange([FromQuery] DateTime startDate, DateTime endDate)
+        {
+            var response = await _httpClient.GetAsync($"?search=date_submitted:[{startDate.ToString("yyyy/MM/dd").Replace("/", "")}+TO+{endDate.ToString("yyyy/MM/dd").Replace("/", "")}]");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<TobaccoResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+
+        [HttpGet("tobaccos-count-by-field")]
+        public async Task<IActionResult> CountTobaccosProduct([FromQuery] string search_field)
+        {
+            var response = await _httpClient.GetAsync($"?count={search_field.ToLower()}.exact");
+            var responseResult = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<TobaccoCountResponse>(
+                responseResult,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return Ok(result);
+        }
+*/
+
+    }
+}
